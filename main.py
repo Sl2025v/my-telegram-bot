90af6a77680b1a7a6cb98d69ef11ccc82e2fcca7
Sl2025v <cnapgajsinformi@gmail.com>
2025-06-24 10:16:14 +0300

    Explicitly set port 8080 and ensure Webhook only

--- main.py
+++ main.py
@@ -57,5 +57,5 @@ if __name__ == '__main__':
             on_shutdown=on_shutdown,
             skip_updates=True,
             host='0.0.0.0',
-            port=8080
+            port=8080  # Явно вкажи порт
         )
